cb287b5210777da649663a452a4aef58c9b92baa
vtarun <vivektarun89@gmail.com>
2018-02-01 20:58:55 +0530

    done

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ angular.module('myApp', [])
 	.controller('myCtrl', function($scope){
 		$scope.leftList = ['Data 1', 'Data 2', 'Data 3', 'Data 4', 'Data 5', 'Data 6', 'Data 7'];
 		$scope.rightList = [];
-		$scope.test = 'working';
-		console.log($scope.test);
+
 		$scope.onRight = function(index){
 			$scope.rightList.push($scope.leftList[index]);
 			$scope.leftList.splice(index,1);
